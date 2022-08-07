286ce8248a6053d5338e87b299877cc654b57b06
Dishi07 <0br3874w24.3c5z@ezweb.ne.jp>
2022-08-08 00:02:33 +0900

    finish section 1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
-var greeting = "Hello, playground"
+import Combine
+
+
+enum MyError: Error {
+    case failed
+}
+
+
+let subject = PassthroughSubject<String, MyError>()
+
+subject.sink(
+    receiveCompletion: { completion in
+        print("recieved completion: ", completion)
+    },
+    receiveValue: { value in
+        print("recieved value: ", value)
+    }
+)
+
+
+subject.send("a")
+subject.send("i")
+subject.send("u")
+subject.send("e")
+subject.send("o")
+subject.send(completion: .failure(.failed))
+subject.send("aiueod")
